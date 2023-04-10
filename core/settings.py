37082897021a13713d15f13e250beee8496846ce
wezlleyv <weslleyonline72@outlook.com>
2023-04-09 23:35:22 -0300

    feat: fixed bug upload image

--- core/settings.py
+++ core/settings.py
@@ -121,8 +121,8 @@ LOGIN_REDIRECT_URL = "home"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media', 'uploads')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles")
